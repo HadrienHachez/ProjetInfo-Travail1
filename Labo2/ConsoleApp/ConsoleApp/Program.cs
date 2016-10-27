42d7254466bd000e58933642c44f69873827aa21
Marcin <13169@student.ecam.be>
2016-10-27 04:47:35 +0200

    commit

--- Labo2/ConsoleApp/ConsoleApp/Program.cs
+++ Labo2/ConsoleApp/ConsoleApp/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleApp
             // globalPath est le dossier où se trouvent les fichiers Students.txt, Teachers.txt, Activites.txt
             // ainsi que le dossier contenant les notes pour chaque unité
 
-            string globalPath = @"H:\ProjetInfo-Travail1\Labo2\Data\";
+            string globalPath = @"C:\Users\marci\Desktop\Data\";
             string notesPath = globalPath + @"Notes\";
             string outputPath = globalPath + @"Output\";
 
@@ -30,43 +30,6 @@ namespace ConsoleApp
             while(HumanInterface.running)
                 HumanInterface.Display();     
 
-            Console.ReadLine();
-        }
-
-        private static StringBuilder DictToString(Dictionary<string, Teacher> dict)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var person in dict.Values)
-            {
-                sb.AppendLine(person.DisplayName());
-            }
-
-            return sb;
-        }
-
-        private static StringBuilder DictToString(Dictionary<string, Student> dict)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var person in dict.Values)
-            {
-                sb.AppendLine(person.DisplayName());
-            }
-
-            return sb;
-        }
-
-        private static StringBuilder DictToString(Dictionary<string, Course> dict)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var course in dict.Values)
-            {
-                sb.AppendLine(course.ToString());
-            }
-
-            return sb;
         }
 
 
